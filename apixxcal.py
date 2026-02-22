e473096056c303ac32e932355e8554866fffb0d6
Adrien Laviron <adrien.laviron@nasa.gov>
2026-02-22 00:37:47 -0500

    Feat (green) automatic many-gaussian fit, not satisfactory

--- apixxcal.py
+++ apixxcal.py
@@ -56,7 +56,7 @@ class AnalyticModel:
 
 if __name__ == "__main__" and 0:
   apix = pd.read_csv("~/astropix-data/20250729-174419_matched.csv")
-  yapix, bins = np.histogram(apix.row_tot[(apix.layer==1)&(apix.chipID==0)&(apix.row==19)&(apix.col==18)], np.arange(0, 4000, 20))
+  yapix, bins = np.histogram(apix.row_tot[(apix.layer==1)&(apix.chipID==0)&(apix.row==19)&(apix.col==18)], np.arange(200, 1000, 20))
   yerr = np.sqrt(np.where(yapix>0, yapix, 1))
   xaxis = .5*(bins[:-1]+bins[1:])
   plt.errorbar(xaxis, yapix, yerr=yerr)
@@ -77,9 +77,29 @@ if __name__ == "__main__" and 0:
 def gaussm(x, x0, h, sigma):
   return h*np.exp( - (x-x0)**2 /2 /sigma**2 )
 
-def fit1gauss(x, data):
-  pass
+def fitgauss(x, data, yerr, model=lambda x:0, p0s=[[]], popts=[[]], pcovs=[[]]):#That was fun, but it doesn’t quite work
+  # Compute residuals
+  res = data - model(x, *popts[-1])
+  chi2 = np.sum(np.square(res/yerr))
+  print(f"chi2={chi2}, ndof={len(data)-len(popts[-1])}")
+  if len(popts[-1]) > len(data)-3 or chi2/(len(data)-len(popts[-1])) < 1:# chi2 stop condition
+    print("Fit done.")
+    return p0s, popts, pcovs
+  else:# refit with one more gaussian
+    print("Adding one gaussian to the model")
+    def f(x, *pars):
+      return model(x, *pars[:-3]) + gaussm(x, *pars[-3:])
+    p0=[x[np.argmax(res)], np.max(res), 5]
+    popt, pcov = curve_fit(f, x, data, sigma=yerr, absolute_sigma=True, p0=np.hstack((popts[-1],p0)))
+    popts.append(popt)
+    pcovs.append(pcov)
+    p0s.append(p0)
+    # plot model so far
+    plt.errorbar(x, data, yerr=yerr, label="data")
+    plt.plot(x, f(x, *popt), label="model")
+    for pars in popts[-1].reshape(int(len(popts[-1])/3), 3):
+      plt.plot(x, gaussm(x, *pars), "--")
+    plt.grid(True, "both"); plt.show()
+    return fitgauss(x, data, yerr, f, p0s, popts, pcovs)
 
-if __name__ == "__main__" and 1:
-  pass
 
